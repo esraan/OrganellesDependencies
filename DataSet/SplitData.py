8ee1f4b514d7bfc80eef1ebd1603074cdf941593
esraan <esraa.mansuor@gmail.com>
2023-06-13 09:55:28 +0300

    metadata_rearrangment

--- DataSet/SplitData.py
+++ DataSet/SplitData.py
@@ -1,3 +1,43 @@
 from CellImagesDataSet import CellImagesDataSet
 import torch
 
+from FitModel.DModel import *
+import os
+import pandas as pd
+from torch.utils.data import DataLoader
+
+
+def split_data(images_dir="/storage/users/g-and-n/plates/metadata/",
+               metadata_dir="/storage/users/g-and-n/plates/images/", plates=["24278", "24277"],
+               target_dir="/sise/home/esraan/metadata_ctrl_trt/"):
+    metadata_plate_paths = metadata_plates_gen(images_dir, plates, metadata_dir)
+    total_ctrl_metadata = pd.DataFrame()
+    for metadata_plate in metadata_plate_paths:
+        curr_df = pd.read_csv(metadata_plate)
+        curr_df["main_path"] = metadata_plate.rstrip(".csv") + "/"
+        total_ctrl_metadata = pd.concat([total_ctrl_metadata, curr_df[curr_df["Well_Role"] == "mock"]])
+        del curr_df
+    # return total_ctrl_metadata
+    total_ctrl_metadata.to_csv(target_dir + "total_ctrl_metadata.csv")
+
+    # mask_paths = [os.path.join(masks_dir, image_id) for image_id in sorted(os.listdir(masks_dir))]
+    print("g")
+
+
+def metadata_plates_gen(images_dir, plates, metadata_dir):
+    metadata_plate_paths = [os.path.join(images_dir, plate + ".csv") for plate in sorted(os.listdir(metadata_dir)) if
+                            plate in plates]
+    for plate_path in metadata_plate_paths:
+        yield plate_path
+
+
+def image_path_mock(image):
+    pass
+
+
+# train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+# test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+df = split_data()
+
+print("here!!!!")
--- FitModel/train.py
+++ FitModel/train.py
@@ -2,15 +2,17 @@ from DModel import *
 import os
 import pandas as pd
 from torch.utils.data import DataLoader
-def split_data(images_dir="/storage/users/g-and-n/plates/metadata/", metadata_dir="/storage/users/g-and-n/plates/images/",plates=["24278","24277"]):
-    total_ctrl_metadata = pd.DataFrame()
+def split_data(images_dir="/storage/users/g-and-n/plates/metadata/", metadata_dir="/storage/users/g-and-n/plates/images/",plates=["24278","24277"], target_dir="/sise/home/esraan/metadata_ctrl_trt/"):
+    
     metadata_plate_paths = metadata_plates_gen(images_dir,plates,metadata_dir)
-    total_df = pd.DataFrame()
+    total_ctrl_metadata = pd.DataFrame()
     for metadata_plate in metadata_plate_paths:
         curr_df = pd.read_csv(metadata_plate)
         curr_df["main_path"] = metadata_plate.rstrip(".csv")+"/"
-        total_df = pd.concat([total_df, curr_df[curr_df["Well_Role"] == "mock"]])
-    return total_df
+        total_ctrl_metadata = pd.concat([total_ctrl_metadata, curr_df[curr_df["Well_Role"] == "mock"]])
+        del curr_df
+    #return total_ctrl_metadata
+    total_ctrl_metadata.to_csv(target_dir+"total_ctrl_metadata.csv")
         
         
         
@@ -30,4 +32,5 @@ def image_path_mock(image):
 # test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 
 df = split_data()
+
 print("here!!!!")
